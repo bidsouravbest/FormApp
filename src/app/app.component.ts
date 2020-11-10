88b3885749cddac43a3a2ce5e0d0b2ec1c1dab80
bidsouravbest <souravbid321@gmail.com>
2020-11-10 22:42:51 +0530

    one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { User } from './User';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'project1';
+export class AppComponent 
+{
+  userModel : User = new User('' , '' , null , '');
+
+  isFormEmpty : boolean = true;
+
+  users : User[] = [];
+
+  onSubmit(userForm : NgForm)
+  {
+    this.isFormEmpty = false;
+    this.users.push(userForm.value);
+    
+
+    // userForm.reset();
+  }
+
 }
